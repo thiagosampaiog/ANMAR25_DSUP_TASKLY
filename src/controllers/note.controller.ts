288aff0a707176171c2233e0e99bb30e7d81588b
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-04-15 17:32:01 -0300

    fix: typescript error No overload matches this call

--- src/controllers/note.controller.ts
+++ src/controllers/note.controller.ts
@@ -1,79 +1,74 @@
 import { Request, Response } from "express";
 import { NoteService } from "../services/note.service";
 
-
 export class NoteController {
+  static async create(req: Request, res: Response) {
+    try {
+      const taskId = Number(req.params.taskId);
 
-     static async create(req: Request, res: Response) {
-          try {
-               const taskId = Number(req.params.taskId);
-     
-               const note = await NoteService.createNote(taskId, req.body);
-     
-               return res.status(201).json(note)
-          }
-           catch (error: any) {
-               return res.status(400).json({ message: error.message })
-          }
- }
+      const note = await NoteService.createNote(taskId, req.body);
 
+      res.status(201).json(note);
+      return;
+    } catch (error: any) {
+      res.status(400).json({ message: error.message });
+      return;
+    }
+  }
 
+  static async getNotesByTask(req: Request, res: Response) {
+    try {
+      const taskId = Number(req.params.taskId);
 
- static async getNotesByTask(req: Request, res: Response) {
-     try {
-          const taskId = Number(req.params.taskId);
+      const notes = await NoteService.getAllNotesByTask(taskId);
 
-          const notes = await NoteService.getAllNotesByTask(taskId);
+       res.json(notes);
+       return;
+    } catch (error: any) {
+       res.status(500).json({ message: error.message });
+       return;
+    }
+  }
 
-          return res.json(notes)
-     }
-      catch (error: any) {
-          return res.status(500).json({ message: error.message })
-     }
-}
+  static async getNote(req: Request, res: Response) {
+    try {
+      const id = Number(req.params.id);
 
+      const note = await NoteService.getNoteById(id);
 
+       res.json(note);
+       return;
+    } catch (error: any) {
+       res.status(404).json({ message: error.message });
+       return;
+    }
+  }
 
-static async getNote(req: Request, res: Response) {
-     try {
-          const id = Number(req.params.id);
+  static async update(req: Request, res: Response) {
+    try {
+      const id = Number(req.params.id);
 
-          const note = await NoteService.getNoteById(id);
+      const updatedNote = await NoteService.updateNote(id, req.body);
 
-          return res.json(note)
-     }
-      catch (error: any) {
-          return res.status(404).json({ message: error.message })
-     }
-}
+       res.status(200).json(updatedNote);
+       return;
+    } catch (error: any) {
+       res.status(400).json({ message: error.message });
+       return;
+    }
+  }
 
+  static async delete(req: Request, res: Response) {
+    try {
+      const id = Number(req.params.id);
 
+      await NoteService.deleteNote(id);
 
-static async update(req: Request, res: Response) {
-     try {
-          const id = Number(req.params.id);
-
-          const updatedNote = await NoteService.updateNote(id, req.body);
-
-          return res.status(200).json(updatedNote)
-     }
-      catch (error: any) {
-          return res.status(400).json({ message: error.message })
-     }
-}
-
-
-
-static async delete(req: Request, res: Response) {
-     try {
-          const id = Number(req.params.id);
-
-          await NoteService.deleteNote(id);
-
-          return res.status(204).send();
-     }
-      catch (error: any) {
-          return res.status(400).json({ message: error.message })
-     }
-}
+       res.status(204).send();
+       return;
+    } catch (error: any) {
+       res.status(400).json({ message: error.message });
+       return;
+    }
+  }
 }
