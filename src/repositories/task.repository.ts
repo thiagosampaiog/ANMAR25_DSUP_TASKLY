41d7b5cfac3b0a72377973a90cf03f57306f03f0
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-04-15 14:20:19 -0300

    fix: TaskRepository update

--- src/repositories/task.repository.ts
+++ src/repositories/task.repository.ts
@@ -2,20 +2,29 @@ import { prisma } from "../database/prisma";
 
 export class TaskRepository {
   static async create(data: { title: string; description: string }) {
-    // criar
     return prisma.task.create({ data });
   }
 
   static async findAll() {
-    // listar todos
     return prisma.task.findmany();
   }
 
-  static async findById(id: string) {
-    return prisma.task.findUnique({ where: { id } }); // achar um
+  static async findById(id: number) {
+    return prisma.task.findUnique({ where: { id } }); 
   }
 
-  static async delete(id: string) {  // deletar
+  static async delete(id: number) {
     return prisma.task.delete({ where: { id } });
   }
+
+  static async update(data: { title: string; description: string; id: number }){
+     return prisma.task.update({
+          where: { id: data.id },
+          data: {
+               title: data.title,
+               description: data.description
+          }
+     });
+  }
 }
+
