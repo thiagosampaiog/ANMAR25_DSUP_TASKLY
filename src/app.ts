30293446365be3079a1bfe0e42a1bbf536241996
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-04-15 17:46:45 -0300

    chore: removing output from prisma schema

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,20 @@
 import express from "express";
-import taskRoutes from './routes/task.routes';
+import taskRoutes from "./routes/task.routes";
+import noteRoutes from "./routes/note.routes"
 
 const port = 3000;
 
 const app = express();
 
-
 app.use(express.json());
 
 app.use(taskRoutes);
+app.use(noteRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-    console.log(`Server is running at ${port}`)
-})
+  console.log(`Server is running at ${port}`);
+});
