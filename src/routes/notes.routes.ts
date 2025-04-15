02119257ca6fa16b89b1d4aa982c9ec759a29ed1
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-04-15 17:30:14 -0300

    feat: creating notesRoutes

--- src/routes/notes.routes.ts
+++ src/routes/notes.routes.ts
@@ -1,3 +1,14 @@
 import { Router } from "express";
+import { NoteController } from "../controllers/note.controller";
 
 const router = Router();
+
+
+router.post('/tasks/:taskId/notes', NoteController.create) 
+router.get('/tasks/:taskId/notes', NoteController.getNotesByTask) 
+router.get('/notes/:id', NoteController.getNote) 
+router.put('/notes/:id', NoteController.update) 
+router.delete('/notes/:id', NoteController.delete) 
+
+
+export default router;
