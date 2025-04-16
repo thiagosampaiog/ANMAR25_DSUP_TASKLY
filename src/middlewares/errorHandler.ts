1927e2a70443f4508714415e8d0d033dd1b1318c
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-04-16 19:09:11 -0300

    feat: implementing application error and error handler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,2 +1,24 @@
+import { ZodError } from "zod";
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "./appError";
 
+export const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof ZodError) {
+    return res.status(400).json({
+      message: "Validation Error",
+      errors: err.errors,
+    });
+  }
 
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  console.error("Internal Error", err);
+  return res.status(500).json({ message: "Internal Server Error" });
+};
