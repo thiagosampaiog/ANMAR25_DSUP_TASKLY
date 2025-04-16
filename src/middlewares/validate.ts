1d543cf43ae001131d8eee9861717987f18db1cc
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-04-16 14:34:54 -0300

    feat: created validate middleware

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,4 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+import { AnyZodObject } from "zod";
+import { ZodError } from "zod";
 
-
-
-
+export const validateData =
+  (schema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+ 
+      req.body = await schema.parseAsync(req.body);
+      next();
+    
+  };
--- src/schemas/note.schemas.ts
+++ src/schemas/note.schemas.ts
@@ -1,8 +1,5 @@
 import { z } from "zod";
 
-
-
-
 export const createNoteSchema = z.object({
      content: z.string().min(1, 'Content is Required'),
      taskId: z.number()
