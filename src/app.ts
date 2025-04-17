4f222d75daeacc6c86ab6ed6d0c7cb6c33749c61
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-04-17 16:57:01 -0300

    feat: add global error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import taskRoutes from "./routes/task.routes";
 import noteRoutes from "./routes/note.routes";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const port = 3000;
 
@@ -14,6 +15,8 @@ app.use(noteRoutes);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -9,16 +9,19 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof ZodError) {
-    return res.status(400).json({
+    res.status(400).json({
       message: "Validation Error",
       errors: err.errors,
     });
+    return;
   }
 
   if (err instanceof AppError) {
-    return res.status(err.statusCode).json({ message: err.message });
+   res.status(err.statusCode).json({ message: err.message });
+    return;
   }
 
   console.error("Internal Error", err);
-  return res.status(500).json({ message: "Internal Server Error" });
+ res.status(500).json({ message: "Internal Server Error" });
+  return;
 };
